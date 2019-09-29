33dcbee857c63fc08b17f4b41131e4622646e579
kyotocoaching <55943404+kyotocoaching@users.noreply.github.com>
2019-09-29 17:58:50 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function(){
+
+
+  $('.Toggle').click(function() {
+  　　$(this).toggleClass('active');
+
+      $('.btns').toggleClass('appear')
+
+
+  　if ($(this).hasClass('active')) {
+  　　$('.NavMenu').addClass('active');　
+  　} else {
+  　　$('.NavMenu').removeClass('active');
+  　}
+  　});
+
+
+});
